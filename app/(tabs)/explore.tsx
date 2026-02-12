65a8ef14a41900bb83f97d6696bcc3338af66351
girmatesfaye <girmaglory@gmail.com>
2026-02-11 22:27:04 -0800

    updating nativewind file

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -2,5 +2,5 @@ import { Image } from "expo-image";
 import { Platform, StyleSheet, View } from "react-native";
 
 export default function TabTwoScreen() {
-  return <View>This is explore tab</View>;
+  return <View>This is explore tab and thing staff</View>;
 }
